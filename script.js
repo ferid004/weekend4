b5bcf7901270eecb75a74fedfeecb64bb865e998
ferid aqa <afe681a2@gmail.com>
2023-10-16 00:48:36 +0400

    a

---
+++ script.js
@@ -0,0 +1,69 @@
+// 1
+
+// const arr=[1,4,7,2]
+// let max=0
+// let min=9
+// let cem=0
+
+// ardicilliqla duzek
+
+// arr.sort(function(a, b){return a-b});
+// console.log(arr);
+
+// // evvelinden bini silek
+// arr.pop()
+// // axirindanda birini silek
+// arr.shift()
+
+
+// // cemliyek
+// for (let i = 0; i < arr.length; i++) {
+//     cem += arr[i];
+// }
+// console.log(cem);
+
+//2
+
+
+
+
+// 3
+
+// const arr=[1,4,7,2]
+// let max=0
+
+// let cem=0
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i]>max) {
+//         max=arr[i]
+//     }
+//     arrnew=arr.slice(1,i)
+    
+// }
+// for (let j = 0; j < arrnew.length; j++) {
+// cem += arrnew[j];
+// }
+// console.log(cem);
+
+
+
+
+// 4 
+// const arr=[1,4,7,2]
+// let max=0
+// let min=9
+// let cem=0
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i]>max) {
+//         max=arr[i]
+//     }
+//     if (arr[i]<min) {
+//         min=arr[i]
+//     }
+//     cem=(max+min)/2
+// }
+
+
+// console.log(cem);
