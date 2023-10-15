6a6a407a75f1858512dd0c0c23d0246e53bb1e60
ferid aqa <afe681a2@gmail.com>
2023-10-16 01:40:45 +0400

    d

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // 1
+/*
+###############################
+bu 1 de yazdığım biraz tam siz istədiyinizkimi olmuya bilər 
+amma işləyir və mənim ağlıma bunu belə həll eləmək gəldi :)
+###############################
+ */
 
-const arr=[1,4,7,2]
-let max=0
-let min=9
-let cem=0
+// const arr=[1,4,7,2]
+// let max=0
+// let min=9
+// let cem=0
 
 //ardicilliqla duzek
 
@@ -23,6 +29,18 @@ let cem=0
 // console.log(cem);
 
 //2
+
+// let eded=3
+// let ededsayi=0
+// let arr=[1,3,4,4,5,3,2,2,2,2,3,3,5]
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i]===eded) {
+//         ededsayi++;
+//     }
+    
+// }
+// console.log(ededsayi);
 
 
 
